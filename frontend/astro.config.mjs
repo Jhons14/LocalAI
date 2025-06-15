1c9a2d88ec181b6003276faafd36385299047b6f
Jhons14 <tivenorjuela7@gmail.com>
2025-06-15 18:08:39 -0500

    Adding astro.config file

---
+++ frontend/astro.config.mjs
@@ -0,0 +1,22 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import svgr from 'vite-plugin-svgr';
+
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss(), svgr(), react()],
+    preview: {
+      allowedHosts: ['.ngrok-free.app'], // o el dominio exacto que te dio ngrok
+    },
+    server: {
+      allowedHosts: ['.ngrok-free.app'],
+    },
+  },
+
+  integrations: [react()],
+});
